9f92651ba5b3f65abf03a41076cb5b45095b0412
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-10-19 14:54:45 +0530

    Update StringCalculator to detect negative numbers

--- stringCalculator.mjs
+++ stringCalculator.mjs
@@ -10,6 +10,10 @@ export class StringCalculator {
         }
       
         const numArray = numbers.split(delimiter).map(Number);
+        const negatives = numArray.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+        }
         return numArray.reduce((sum, num) => sum + num, 0);            
     }
 }

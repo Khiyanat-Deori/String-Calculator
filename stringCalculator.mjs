b3b504b95c6c4d2485e3f2a28951e0909dd19021
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-10-19 14:36:20 +0530

    Update StringCalculator to support different delimiters

--- stringCalculator.mjs
+++ stringCalculator.mjs
@@ -2,7 +2,14 @@ export class StringCalculator {
     add(numbers) {
         if (numbers === "") return 0;
 
-        const numArray = numbers.split(/[\n,]/).map(Number);
-        return numArray.reduce((sum, num) => sum + num, 0);        
+        let delimiter = /[\n,]/;
+        if (numbers.startsWith('//')) {
+          const delimiterEndIndex = numbers.indexOf('\n');
+          delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+          numbers = numbers.substring(delimiterEndIndex + 1);
+        }
+      
+        const numArray = numbers.split(delimiter).map(Number);
+        return numArray.reduce((sum, num) => sum + num, 0);            
     }
 }

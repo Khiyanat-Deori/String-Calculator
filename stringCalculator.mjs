74b939fe45ac56a3e0c1a93bd8a7a18716be9c52
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-10-19 13:54:50 +0530

    Update StringCalculator to add one or two numbers

--- stringCalculator.mjs
+++ stringCalculator.mjs
@@ -1,6 +1,16 @@
 export class StringCalculator {
     add(numbers) {
         if (numbers === "") return 0;
+
+        const numArray = numbers.split(",");
+
+        if (numArray.length === 1) {
+            return Number(numArray[0]);
+        }
+
+        if (numArray.length === 2) {
+            return Number(numArray[0]) + Number(numArray[1]);
+        }
         
     }
 }

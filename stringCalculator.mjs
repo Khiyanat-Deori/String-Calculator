4abbd2861ac11e733bafb9076abc4fae9cc7a181
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-10-19 14:04:37 +0530

    Update StringCalculator to handle unknown amount of numbers

--- stringCalculator.mjs
+++ stringCalculator.mjs
@@ -2,15 +2,7 @@ export class StringCalculator {
     add(numbers) {
         if (numbers === "") return 0;
 
-        const numArray = numbers.split(",");
-
-        if (numArray.length === 1) {
-            return Number(numArray[0]);
-        }
-
-        if (numArray.length === 2) {
-            return Number(numArray[0]) + Number(numArray[1]);
-        }
-        
+        const numArray = numbers.split(',').map(Number);
+        return numArray.reduce((sum, num) => sum + num, 0);        
     }
 }

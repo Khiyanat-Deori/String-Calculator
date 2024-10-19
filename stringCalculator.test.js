dfc59916ea49d08ca6e13c56a098fbbce06a17ad
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-10-19 13:46:20 +0530

    Test case 2: addition of one or two numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -6,4 +6,12 @@ describe('addition', () => {
   it('should return 0 for an empty string', () => {
     expect(calculator.add("")).to.equal(0);
   });
+  
+  it('should return the number for single input', () => {
+    expect(calculator.add("1")).to.equal(1);
+  });
+  
+  it('should return the sum for two numbers', () => {
+    expect(calculator.add("1,2")).to.equal(3);
+  });
 });
